25835633bd36b9a50d7e7ba82fbaa9b52a7c66fe
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-07-01 19:18:22 +0500

    updated

--- Assignments/fb-post/src/App.js
+++ Assignments/fb-post/src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import FacebookPost from './components/views/FacebookPost';
 import profileImg from './assets/profile.jpg'
 function App() {
+  const currentDate = new Date();
+  console.log(currentDate);
+
+  const addComment = () =>{
+    alert("added...!");
+  }
 
   const postInfo = {
     createdBy: 'Mustafa Ilahi',
     avatar: profileImg,
     description: 'No beauty shines brighter than the beauty of a supercar ❤️',
-    images: ['https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80','https://images.khaleejtimes.com/storyimage/KT/20210313/ARTICLE/210319778/AR/0/AR-210319778.jpg','https://i.pinimg.com/736x/fe/3a/39/fe3a391ad4505935c08d21e70f27796a.jpg','https://i.insider.com/58eec10c2d9f6f24008b4eba','https://images.unsplash.com/photo-1527247043589-98e6ac08f56c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80','https://i.ytimg.com/vi/Rk3T__b2mDg/maxresdefault.jpg']
+    images: ['https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80','https://images.khaleejtimes.com/storyimage/KT/20210313/ARTICLE/210319778/AR/0/AR-210319778.jpg','https://i.pinimg.com/736x/fe/3a/39/fe3a391ad4505935c08d21e70f27796a.jpg','https://i.insider.com/58eec10c2d9f6f24008b4eba','https://images.unsplash.com/photo-1527247043589-98e6ac08f56c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80','https://i.ytimg.com/vi/Rk3T__b2mDg/maxresdefault.jpg'],
+    createdAt: currentDate
   }
   return (
     <div className="App">
-      <FacebookPost postInfo={postInfo}/>
+      <FacebookPost postInfo={postInfo} addComment={addComment}/>
       </div>
   );
 }
--- Assignments/fb-post/src/components/views/FacebookPost/index.js
+++ Assignments/fb-post/src/components/views/FacebookPost/index.js
@@ -1,8 +1,22 @@
 import FbImageLibrary from 'react-fb-image-grid'
 import './index.css'
 import activeStatus from '../../../assets/online-dot.png';
+import { useState } from 'react';
+
 function FacebookPost (props){
     const {postInfo: {createdBy,avatar,description,images,createdAt}} = props
+    const [comment, setComment] = useState('');
+    const [text, setText] = useState([]);
+    const getValue = (e) => {
+        setText(e.target.value)
+    }
+    const addComment = () => {
+        const tempList = [...comment];
+        tempList.push(text);
+        setComment(tempList);
+        setText('');
+    }
+    console.log(comment)
     return(
         <div className="main-div">
             <br/>
@@ -12,6 +26,31 @@ function FacebookPost (props){
             <p id="description">{description}</p>
             <div className="all-images">
             <FbImageLibrary images={images}/>
+            <button>Like</button>
+            <button>Comment</button>
+            <button>Share</button>
+            <br /><br />
+            <div>
+                <input placeholder="Write a comment..." value={text} onChange={(e)=> {getValue(e)}}/>
+                <button onClick={addComment}>
+                    <img src="https://img.icons8.com/material-sharp/24/000000/filled-sent.png"/>
+                </button>
+            
+            </div>
+            </div>
+            <div>
+                {comment ? 
+                    <table style={{margin: "0 auto"}}>
+                        {comment.map(items=>{
+                            return <tr>{items}
+                            <button>Edit</button>
+                            <button>Delete</button>
+                            </tr>
+                        })}
+                    </table>
+                :   
+                    <></>
+                }
             </div>
         </div>
     )
