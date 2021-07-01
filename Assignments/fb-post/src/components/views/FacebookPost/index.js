18c4ddcaa278c82f2bbe709dcbfc5ddbd43611cc
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-07-02 01:54:42 +0500

    completed

--- Assignments/fb-post/src/components/views/FacebookPost/index.js
+++ Assignments/fb-post/src/components/views/FacebookPost/index.js
@@ -7,6 +7,9 @@ function FacebookPost (props){
     const {postInfo: {createdBy,avatar,description,images,createdAt}} = props
     const [comment, setComment] = useState('');
     const [text, setText] = useState([]);
+    const [editMode, setEditMode] = useState(false);
+    const [currentEditIndex, setCurrentEditIndex] = useState(-1);
+
     const getValue = (e) => {
         setText(e.target.value)
     }
@@ -16,37 +19,83 @@ function FacebookPost (props){
         setComment(tempList);
         setText('');
     }
-    console.log(comment)
+    // console.log(comment)
+
+    const editComment = (index) => {
+        // console.log(index)
+        setText(comment[index]);
+        setEditMode(true);
+        setCurrentEditIndex(index);
+    }
+    // console.log(text)
+
+    const updateComment = () => {
+        const tempList = [...comment];
+        tempList.splice(comment,1,text);
+        setComment(tempList);
+        setEditMode(false);
+        setCurrentEditIndex(-1);
+        setText('')
+    }
+
+    const deleteComment = (index) => {
+        const tempList = [...comment];
+        tempList.splice(index,1);
+        setComment(tempList);
+    }
+    // console.log("====>",createdAt)
     return(
         <div className="main-div">
-            <br/>
+            {/* <br/> */}
+            <p className="created-at">{createdAt}</p>
             <img src={avatar} className="profile-img"/>
             <h4 id="created-by">&nbsp; &nbsp; {createdBy}</h4>
             <img src={activeStatus} className="dot"/>
             <p id="description">{description}</p>
             <div className="all-images">
             <FbImageLibrary images={images}/>
-            <button>Like</button>
-            <button>Comment</button>
-            <button>Share</button>
+            <div id="icons">    
+                <div id="like-icon">
+                    <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Facebook_Thumb_icon.svg/1200px-Facebook_Thumb_icon.svg.png" height="50px"/>
+                    <span>Like &nbsp; &nbsp;</span>
+                </div>
+                <div id="comment-icon">
+                    <img src="https://viplikes.net/img/catalog_sect_img/new/facebook%20comment%20icon.png" height="30px"/> &nbsp; &nbsp;
+                    <span>Comment</span>
+                </div>
+                <div id="share-icon">
+                    <img src="https://purepng.com/public/uploads/large/share-icon-7nl.png" height="30px"/>
+                    <span>&nbsp; Share</span>
+                </div>
+            </div>
             <br /><br />
             <div>
-                <input placeholder="Write a comment..." value={text} onChange={(e)=> {getValue(e)}}/>
-                <button onClick={addComment}>
-                    <img src="https://img.icons8.com/material-sharp/24/000000/filled-sent.png"/>
-                </button>
+                <input placeholder="Write a comment..." value={text} onChange={(e)=> {getValue(e)}} id="input-field"/>
+                {
+                    editMode ? <button onClick={updateComment}>Update</button> 
+                    :
+                    <button onClick={addComment} className="send-btn">
+                        <img src="https://img.icons8.com/material-sharp/24/000000/filled-sent.png"/>
+                    </button>
+                }
             
             </div>
             </div>
             <div>
                 {comment ? 
                     <table style={{margin: "0 auto"}}>
-                        {comment.map(items=>{
-                            return <tr>{items}
-                            <button>Edit</button>
-                            <button>Delete</button>
+                        {comment.map((items,index)=>{
+                            return (
+                                <tr key={index}>
+                                <h5 className="comment-user">{createdBy}</h5>
+                                <img src={avatar} className="comment-img"/> 
+                                &nbsp; &nbsp;   
+                                {items} 
+                                <button className="edit-btn" onClick={()=> editComment(index)}>Edit</button>
+                                <button className="del-btn" onClick={() => deleteComment(index) }>Delete</button>
                             </tr>
-                        })}
+                        )
+                    })}
                     </table>
                 :   
                     <></>
